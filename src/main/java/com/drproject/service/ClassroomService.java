3824af40860b2a0bc342a5f04dce7ae5b684a269
Ilya <iatashsowda@gmail.com>
2024-01-30 09:39:40 +0330

    teacher can now add new topic and change topic name

--- src/main/java/com/drproject/service/ClassroomService.java
+++ src/main/java/com/drproject/service/ClassroomService.java
@@ -4,6 +4,7 @@ import com.drproject.entity.*;
 import com.drproject.repository.ARRepository;
 import com.drproject.repository.ClassroomRepository;
 import com.drproject.repository.UserRepository;
+import org.hibernate.sql.results.graph.entity.internal.AbstractNonJoinedEntityFetch;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -217,119 +218,107 @@ public class ClassroomService {
         return res;
     }
 
-        public HashMap<String, Object> getStudentGradeForClass(String username, String classroomCode){
-            HashMap<String, Object> res = new HashMap<>();
-            User user = userRepository.getUserByUsername(username);
-            double score = 0;
+    public HashMap<String, Object> getStudentGradeForClass(String username, String classroomCode){
+        HashMap<String, Object> res = new HashMap<>();
+        User user = userRepository.getUserByUsername(username);
+        double score = 0;
 
-            if(classroomRepository.existsByCode(classroomCode)){
-                Classroom classroom = classroomRepository.getClassroomByCode(classroomCode);
-                List<Section> sections = classroom.getSections();
-                for(Section s: sections){
-                    HashMap<String,Object> hashMap = getStudentGradeForSection(username,classroomCode,s.getId());
-                    if (hashMap.get("obj").equals(null)){
-                        score += 0;
-                    }
-                    else {
-                        score += Double.parseDouble((String) hashMap.get("obj"));
-                    }
+        if(classroomRepository.existsByCode(classroomCode)){
+            Classroom classroom = classroomRepository.getClassroomByCode(classroomCode);
+            List<Section> sections = classroom.getSections();
+            for(Section s: sections){
+                HashMap<String,Object> hashMap = getStudentGradeForSection(username,classroomCode,s.getId());
+                if (hashMap.get("obj").equals(null)){
+                    score += 0;
                 }
-                String out = "" + (score/classroom.getSections().size() *100);
-                res.put("obj", out);
-                res.put("msg", "classroom score for user returned successfully");
-                res.put("status", true);
-                return res;
+                else {
+                    score += Double.parseDouble((String) hashMap.get("obj"));
+                }
             }
-            res.put("obj", null);
-            res.put("msg", "classroom doesn't exist");
+            String out = "" + (score/classroom.getSections().size() *100);
+            res.put("obj", out);
+            res.put("msg", "classroom score for user returned successfully");
             res.put("status", true);
             return res;
         }
+        res.put("obj", null);
+        res.put("msg", "classroom doesn't exist");
+        res.put("status", true);
+        return res;
+    }
 
-        public HashMap<String,Object> getStudentGradeForSection(String username, String classroomCode, String sectionUUID){
-            HashMap<String, Object> res = new HashMap<>();
-            User user = userRepository.getUserByUsername(username);
-            double score = 0;
+    public HashMap<String,Object> getStudentGradeForSection(String username, String classroomCode, String sectionUUID){
+        HashMap<String, Object> res = new HashMap<>();
+        User user = userRepository.getUserByUsername(username);
+        double score = 0;
 
-            if(classroomRepository.existsByCode(classroomCode)){
-                Classroom classroom = classroomRepository.getClassroomByCode(classroomCode);
-                List<Section> sections = classroom.getSections();
-                for(Section s: sections) {
-                    if (s.getId().equals(sectionUUID)) {
-                        List<Activity> sectionActivities = s.getActivities();
-                        for (Activity a : sectionActivities) {
-                            HashMap<String, Object> hashMappedRes = getStudentGradeForActivity(username, sectionUUID);
-                            score += Double.parseDouble((String) hashMappedRes.get("obj"));
-                        }
+        if(classroomRepository.existsByCode(classroomCode)){
+            Classroom classroom = classroomRepository.getClassroomByCode(classroomCode);
+            List<Section> sections = classroom.getSections();
+            for(Section s: sections) {
+                if (s.getId().equals(sectionUUID)) {
+                    List<Activity> sectionActivities = s.getActivities();
+                    for (Activity a : sectionActivities) {
+                        HashMap<String, Object> hashMappedRes = getStudentGradeForActivity(username, sectionUUID);
+                        score += Double.parseDouble((String) hashMappedRes.get("obj"));
                     }
                 }
+            }
 
-                String out = "" + (score / classroom.getSections().size() * 100);
-                res.put("obj", out);
-                res.put("msg", "returned user score in section");
-                res.put("status", true);
-                return res;
-            }
-            else {
-                res.put("msg", "classroom not found");
-                res.put("status", false);
-                res.put("obj",null);
-                return res;
-            }
+            String out = "" + (score / classroom.getSections().size() * 100);
+            res.put("obj", out);
+            res.put("msg", "returned user score in section");
+            res.put("status", true);
+            return res;
         }
+        else {
+            res.put("msg", "classroom not found");
+            res.put("status", false);
+            res.put("obj",null);
+            return res;
+        }
+    }
 
-        public HashMap<String,Object> getStudentGradeForActivity(String username, String uuid){
-            HashMap<String, Object> res = new HashMap<>();
-            User user = userRepository.getUserByUsername(username);
-            boolean systemCheck = true;
-            if(arRepository.existsById(uuid)){
-                Activity activity = arRepository.getActivityById(uuid);
+    public HashMap<String,Object> getStudentGradeForActivity(String username, String uuid){
+        HashMap<String, Object> res = new HashMap<>();
+        User user = userRepository.getUserByUsername(username);
+        boolean systemCheck = true;
+        if(arRepository.existsById(uuid)){
+            Activity activity = arRepository.getActivityById(uuid);
 
-                if(activity instanceof Quiz){
-                    double score = 0;
-                    Quiz quiz = (Quiz)activity;
-                    List<Question> questions = quiz.getQuestions();
-                    for(Question q : questions){
-                        if (!(q instanceof MultipleAnswer)){
-                            systemCheck = false;
-                        }
+            if(activity instanceof Quiz){
+                double score = 0;
+                Quiz quiz = (Quiz)activity;
+                List<Question> questions = quiz.getQuestions();
+                for(Question q : questions){
+                    if (!(q instanceof MultipleAnswer)){
+                        systemCheck = false;
                     }
-                    if (systemCheck){
-                        for(Question q : questions) {
-                            double questionScore = 0;
-                            if (q instanceof MultipleAnswer) {
-                                List<Choice> choiceList = ((MultipleAnswer) q).getChoiceList();
-                                for (Choice c : choiceList) {
-                                    List<StudentChoice> studentChoiceList = c.getStudentChoices();
-                                    for (StudentChoice s : studentChoiceList) {
-                                        if (s.getUser().equals(user)) {
-                                            questionScore = Double.parseDouble(c.getCorrectness());
-                                        }
+                }
+                if (systemCheck){
+                    for(Question q : questions) {
+                        double questionScore = 0;
+                        if (q instanceof MultipleAnswer) {
+                            List<Choice> choiceList = ((MultipleAnswer) q).getChoiceList();
+                            for (Choice c : choiceList) {
+                                List<StudentChoice> studentChoiceList = c.getStudentChoices();
+                                for (StudentChoice s : studentChoiceList) {
+                                    if (s.getUser().equals(user)) {
+                                        questionScore = Double.parseDouble(c.getCorrectness());
                                     }
                                 }
-                                score += questionScore;
                             }
-                        }
-                        String out = "" +  score / quiz.getQuestions().size() * 100;
-                        res.put("obj", out);
-                        res.put("status", true);
-                        res.put("msg", "grade for quiz (assigned by system) returned successfully");
-                        return res;
-                    }
-                    else{
-                        List<StudentLongAnswer> studentLongAnswers=quiz.getStudentLongAnswers();
-                        for(StudentLongAnswer s : studentLongAnswers){
-                            if(s.getUser().equals(user)){
-                                res.put("obj",s.getGrade());
-                                res.put("status", true);
-                                res.put("msg", "student grade assigned by ostad");
-                                return res;
-                            }
+                            score += questionScore;
                         }
                     }
+                    String out = "" +  score / quiz.getQuestions().size() * 100;
+                    res.put("obj", out);
+                    res.put("status", true);
+                    res.put("msg", "grade for quiz (assigned by system) returned successfully");
+                    return res;
                 }
-                else {
-                    List<StudentLongAnswer> studentLongAnswers=activity.getStudentLongAnswers();
+                else{
+                    List<StudentLongAnswer> studentLongAnswers=quiz.getStudentLongAnswers();
                     for(StudentLongAnswer s : studentLongAnswers){
                         if(s.getUser().equals(user)){
                             res.put("obj",s.getGrade());
@@ -338,17 +327,64 @@ public class ClassroomService {
                             return res;
                         }
                     }
-                    res.put("obj","0");
-                    res.put("status", true);
-                    res.put("msg", "student hasn't participated in activity");
-                    return res;
                 }
             }
-            res.put("status", false);
-            res.put("msg", "activity/classroom does not exist");
-            res.put("obj", null);
-            return res;
+            else {
+                List<StudentLongAnswer> studentLongAnswers=activity.getStudentLongAnswers();
+                for(StudentLongAnswer s : studentLongAnswers){
+                    if(s.getUser().equals(user)){
+                        res.put("obj",s.getGrade());
+                        res.put("status", true);
+                        res.put("msg", "student grade assigned by ostad");
+                        return res;
+                    }
+                }
+                res.put("obj","0");
+                res.put("status", true);
+                res.put("msg", "student hasn't participated in activity");
+                return res;
+            }
         }
+        res.put("status", false);
+        res.put("msg", "activity/classroom does not exist");
+        res.put("obj", null);
+        return res;
+    }
 
-
+    public HashMap<String , Object> updateClassroomSections(String sectionTitle,String classroomCode, String sectionId){
+        HashMap<String, Object> res = new HashMap<>();
+        if(classroomRepository.existsByCode(classroomCode)) {
+            Classroom classroom = classroomRepository.getClassroomByCode(classroomCode);
+            if (sectionId.equals("")){
+                Section newSection = new Section();
+                newSection.setTitle(sectionTitle);
+                classroom.getSections().add(newSection);
+                for(Section s:classroom.getSections()){
+                    s.setClassroom(classroom);
+                }
+                classroomRepository.save(classroom);
+                res.put("obj",null);
+                res.put("msg", "successfully added new section to classroom");
+                res.put("status", true);
+                return res;
+            }
+            else {
+                List<Section> sections = classroom.getSections();
+                for(Section s : sections){
+                    if (s.getId().equals(sectionId)){
+                        s.setTitle(sectionTitle);
+                        classroomRepository.save(classroom);
+                        res.put("obj",null);
+                        res.put("msg", "successfully changed classroomTitle");
+                        res.put("status", true);
+                        return res;
+                    }
+                }
+            }
+        }
+        res.put("obj",null);
+        res.put("msg", "classroom not found");
+        res.put("status", false);
+        return res;
+    }
 }
