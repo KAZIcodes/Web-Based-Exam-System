c19ca18b37a041b57e7c94b37fc15d9263a791aa
Amir@li KaziI <amirkazi3344@yahoo.com>
2024-01-31 04:01:47 +0330

    need sleep

--- src/main/java/com/drproject/controllers/ClassroomController.java
+++ src/main/java/com/drproject/controllers/ClassroomController.java
@@ -169,27 +169,28 @@ public class ClassroomController {
         }
     }
 
-//    @GetMapping("/{classroomId}/grades")
-//    public ResponseEntity<?> updateClassroomTopicGrades(HttpSession session, @PathVariable String classroomId, @RequestBody List<HashMap<String, String>> glossaryList) {
-//        if (session.getAttribute("username") == null){
-//            Map<String, Object> error = new HashMap<>();
-//            error.put("status", false);
-//            error.put("msg", "Sign in first!");
-//            return ResponseEntity.ok(error);
-//        }
-//
-//        /////////////////updateClassroomGlossary method needed in classRoom service which takes new glossary list and updates it for the classroom id it takes
-//        if (classromService.getUserRole((String) session.getAttribute("username"), classroomId).get("obj").equals("teacher")){
-//            Map<String, Object> res = classromService.getTopicGrades(classroomId);
-//            return ResponseEntity.ok(res);
-//        }
-//        else {
-//            Map<String, Object> error = new HashMap<>();
-//            error.put("status", false);
-//            error.put("msg", "You don't have access to this classroom!");
-//            return ResponseEntity.ok(error);
-//        }
-//    }
+    @GetMapping("/{classroomId}/grades")
+    public ResponseEntity<?> updateClassroomTopicGrades(HttpSession session, @PathVariable String classroomId) {
+        if (session.getAttribute("username") == null){
+            Map<String, Object> error = new HashMap<>();
+            error.put("status", false);
+            error.put("msg", "Sign in first!");
+            return ResponseEntity.ok(error);
+        }
+
+        /////////////////updateClassroomGlossary method needed in classRoom service which takes new glossary list and updates it for the classroom id it takes
+        Map<String, Object> res = classromService.getUserRole((String) session.getAttribute("username"), classroomId);
+        if (res.get("obj").equals("teacher") || res.get("obj").equals("admin")){
+            res = ARserivce.getAllGradesForClassroom(classroomId);
+            return ResponseEntity.ok(res);
+        }
+        else {
+            Map<String, Object> error = new HashMap<>();
+            error.put("status", false);
+            error.put("msg", "You don't have access to this classroom!");
+            return ResponseEntity.ok(error);
+        }
+    }
 
     //returns AR data for teacher to tashih or for student to do the exam for example
     @GetMapping("/{classroomId}/AR/{ARid}")
